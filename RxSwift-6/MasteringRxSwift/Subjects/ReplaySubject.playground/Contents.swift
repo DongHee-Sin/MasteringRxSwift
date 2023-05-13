7a43644523922653caaaf9c37e0ceb39e80c8692
dong <ehdgml4191@gmail.com>
2023-05-14 01:13:58 +0900

    RxSwift - ReplaySubject

--- RxSwift-6/MasteringRxSwift/Subjects/ReplaySubject.playground/Contents.swift
+++ RxSwift-6/MasteringRxSwift/Subjects/ReplaySubject.playground/Contents.swift
@@ -35,7 +35,40 @@ enum MyError: Error {
 }
 
 
+/// Publish, Behavior과 다르게 create메서드를 통해 생성
+/// 몇개의 이벤트를 저장하고 사용할지 매개변수로 전달(bufferSize)
+let replay = ReplaySubject<Int>.create(bufferSize: 3)
+
+
+// 10개의 next이벤트 전달
+(1...10).forEach { replay.onNext($0) }
+
+
+/// Observer등록 (buffer에 저장된 이벤트들만 바로 방출)
+replay.subscribe { print("Observer1 : \($0)") }
+    .disposed(by: disposeBag)
+
+replay.subscribe { print("Observer2 : \($0)") }
+    .disposed(by: disposeBag)
+
+
+/// 새로운 next 이벤트를 전달받으면 가장 오래된 이벤트를 삭제
+replay.onNext(11)
 
 
 
+replay.subscribe { print("Observer3 : \($0)") }
+    .disposed(by: disposeBag)
 
+
+/// buffer는 결국 메모리에 저장되어 사용되기 때문에, 꼭 필요한 경우가 아니라면 너무 큰 buffer를 갖지 않도록 해야한다.
+
+
+/// complete, error가 발생해도 새로운 구독이 발생하면 buffer에 저장해둔 이벤트를 방출하고 completed 또는 error 이벤트를 방출한다.
+
+//replay.onCompleted()
+replay.onError(MyError.error)
+
+
+replay.subscribe { print("Observer3 : \($0)") }
+    .disposed(by: disposeBag)
