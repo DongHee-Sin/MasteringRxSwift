58e5fc84a7762008c25a99bf2d969662af5ca231
dong <ehdgml4191@gmail.com>
2023-05-15 00:16:07 +0900

    RxSwift - Relay(Publish, Behavior, Replay)

--- RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
+++ RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
@@ -32,8 +32,40 @@ import RxCocoa
 let bag = DisposeBag()
 
 
+/// Relay는 3가지 종류가 있다. => PublishRelay,BehaviorRelay, ReplayRelay
+/// Relay는 Subject를 래핑한 형태로 구현되어 있으며, Subject와 마찬가지로 이벤트를 전달받아서 자신을 구독하고 있는 Observer에게 방출하는 기능을 한다.
+/// 차이점은 completed, error에 대한 이벤트를 전달받지도 않고, Observer로 전달하지도 않는 것.
+/// 자신을 구독하고 있는 Observer가 Dispose되기 전까지 next이벤트를 무한히 방출한다.
+/// 이러한 특징 때문에 주로 UI 이벤트에 활용된다. (UIButton의 Tap 이벤트, TextView의 Input 이벤트 등..) => RxCocoa
 
 
 
+let publishRelay = PublishRelay<Int>()
+
+publishRelay.subscribe { print("publish : \($0)") }
+    .disposed(by: bag)
+
+publishRelay.accept(1)
 
 
+
+let behaviorRelay = BehaviorRelay<Int>(value: 10)
+behaviorRelay.accept(20)
+
+behaviorRelay.subscribe { print("behavior : \($0)") }
+    .disposed(by: bag)
+
+behaviorRelay.accept(30)
+
+print(behaviorRelay.value)  // BehaviorRelay가 가지고 있는 next이벤트의 value를 가져올 수 있다. (Read only)
+
+
+
+let replayRelay = ReplayRelay<Int>.create(bufferSize: 3)
+
+(1...10).forEach {
+    replayRelay.accept($0)
+}
+
+replayRelay.subscribe { print("replay : \($0)") }
+    .disposed(by: bag)
